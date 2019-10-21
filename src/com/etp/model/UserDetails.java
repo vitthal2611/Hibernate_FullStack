74eea6bccb9d7eaf9b10e555620bad3247f6684f
Your Name <you@example.com>
2019-10-21 20:11:10 +0530

    Hibernate collection examle

--- src/com/etp/model/UserDetails.java
+++ src/com/etp/model/UserDetails.java
@@ -1,67 +1,96 @@
 package com.etp.model;
 
+import java.util.Collection;
 import java.util.Date;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-import javax.annotation.Generated;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-
-@Entity(name="Table420")
+@Entity(name = "Table420")
 public class UserDetails {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	public int userId;
-	
+
 	public String userName;
-	
+
 	public String mobileNumber;
-	
-	@Column(name="dob")
-	@Temporal(TemporalType.DATE)
-	
+
 	public Date birthDate;
-	
-	public List<Address> address;
+
+	@ElementCollection
+	public Set<Address> adr = new HashSet<>();
+
+	public transient String password;
+
 	
 	public int getUserId() {
 		return userId;
 	}
+
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getMobileNumber() {
-		return mobileNumber;
+		return "mr " + mobileNumber;
 	}
+
 	public void setMobileNumber(String mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
+
+	@Column(name = "dob")
+	@Temporal(TemporalType.DATE)
 	public Date getBirthDate() {
 		return birthDate;
 	}
+
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
+
+	
+
+	public Collection<Address> getAdr() {
+		return adr;
+	}
+
+	public void setAdr(Set<Address> adr) {
+		this.adr = adr;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	@Override
 	public String toString() {
 		return "UserDetails [userId=" + userId + ", userName=" + userName + ", mobileNumber=" + mobileNumber
 				+ ", birthDate=" + birthDate + "]";
 	}
-	
-	
-	
 
 }
--- src/com/etp/test/UserDetailsTest.java
+++ src/com/etp/test/UserDetailsTest.java
@@ -1,13 +1,18 @@
 package com.etp.test;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import com.etp.model.Address;
 import com.etp.model.UserDetails;
 
 public class UserDetailsTest {
@@ -19,12 +24,30 @@ public class UserDetailsTest {
 		user.setUserName("Xyz");
 		user.setMobileNumber("9860986035");
 		user.setBirthDate(new Date(156114));
+		user.setPassword("123456");
+
+		Address adr = new Address();
+
+		adr.setCountry("India");
+		adr.setPincode("123456");
+		adr.setStreet("Mahatama Gandhi Road");
+		adr.setDescrption("Pune");
 
 		UserDetails user2 = new UserDetails();
 
 		user2.setUserName("Xyz");
 		user2.setMobileNumber("9860986035");
 		user2.setBirthDate(new Date(156114));
+		user2.setPassword("123456");
+
+		Set<Address> adrList = new HashSet<>();
+
+		adrList.add(adr);
+
+		user.setAdr(adrList);
+		
+
+		user2.setAdr(adrList);
 
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 
@@ -32,13 +55,15 @@ public class UserDetailsTest {
 
 		Transaction transaction = session.beginTransaction();
 
+		session.save(user);
+		session.save(user2);
+
+
 		/*
-		 * session.save(user); session.save(user2);
+		 * user = null;
+		 * 
+		 * user = session.get(UserDetails.class, 1);
 		 */
-
-		user = null;
-
-		user = session.get(UserDetails.class, 1);
 
 		System.out.println(user);
 
